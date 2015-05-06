f10547bfd5f0c24842009fa54335d7a8671ad03c
Simon Lorent <simon.lorent@student.ulg.ac.be>
2015-05-06 18:05:09 +0200

    Ajout des prototype de le .h et debut du codage des fonctions

--- multiset_list.c
+++ multiset_list.c
@@ -3,3 +3,42 @@
 
 #include <assert.h>
 #include <stdlib.h>
+
+struct list
+{
+	void *data;
+	struct list_t *next;
+};
+
+typedef struct list_t cell
+
+list *create_empty()
+{
+	list *L = malloc(sizeof(list));
+	assert(L);
+	L->next = NULL;
+	return L;
+}
+
+bool is_empty(list *L)
+{
+	assert(L);
+	if(L->data)
+		return FALSE;
+	else
+		return TRUE;
+}
+
+int count(list *L, void *element, bool (*compare(const void *, const void *)))
+{
+	assert(L);
+	cell current = L;
+	int count = 0;
+	while(current)
+	{
+		if(compare(current->data, element))
+			count++;
+		current = current->next;
+	}
+	return count;
+}
---
+++ multiset_list.h
@@ -0,0 +1,25 @@
+#ifndef __MULTISET_LIST__
+#define __MULTISET_LIST__
+
+#include "boolean.h"
+#include <stdlib.h>
+
+typedef struct list_t list;
+
+list *create_empty();
+
+bool is_empty(list *L);
+
+int count(list *L, void *element ,bool (*compare(const void *, const void *)));
+
+int occurrences(list *L, void *element, bool (*compare(const void *, const void *)));
+
+bool part_of(list *L, void *element, bool (*compare(const void *, const void *)));
+
+bool equals(list *L1, list *L2, bool (*compare(const void *, const void *)));
+
+list *join(list *L1, list *L2);
+
+list *add_to(list *L, void *element);
+
+list remove_from(list *L, void *element);
